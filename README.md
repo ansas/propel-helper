95a59dc22f85e46be17532212f378f89713f81b1
Ansas <webmaster@ansas-meyer.de>
2014-07-19 12:56:34 +0200

    Added class ReadOnly

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ PropelHelper
 ============
 
 Helper classes for Propel ORM
+
+## ReadOnly
+This trait is a workaround for the bug when setting tables to `readOnly="true"` in Propel's schema.xml and still wanting to use the `joinWith()` methods.
+
+For details see https://github.com/propelorm/Propel2/issues/629
+
+Example usage:
+```php
+<?php
+
+use Base\User as BaseUser;
+use PropelHelper\ReadOnly;
+
+class User extends BaseUser
+{
+    use ReadOnly;
+}
+```
---
+++ src/ReadOnly.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the PropelHelper package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace PropelHelper;
+
+use Propel\Runtime\Connection\ConnectionInterface;
+use Propel\Runtime\Exception\PropelException;
+
+/**
+ * Readonly
+ *
+ * This trait is a workaround for the bug when setting tables to
+ * <code>readOnly="true"</code> in Propel's schema.xml and still
+ * wanting to use the <code>joinWith()</code> methods
+ *
+ * @see: https://github.com/propelorm/Propel2/issues/629
+ *
+ * @author Ansas Meyer <webmaster@ansas-meyer.de>
+ */
+trait Readonly
+{
+    /**
+     * Removes this object from datastore and sets delete attribute.
+     *
+     * @param ConnectionInterface $con
+     * @return void
+     * @throws PropelException
+     */
+    public function delete(ConnectionInterface $con = null)
+    {
+        throw new PropelException("This is a readonly object.");
+    }
+
+    /**
+     * Persists this object to the database.
+     *
+     * @param ConnectionInterface $con
+     * @return int The number of rows affected by this insert/update and any referring fk objects' save() operations.
+     * @throws PropelException
+     */
+    public function save(ConnectionInterface $con = null)
+    {
+        throw new PropelException("This is a readonly object.");
+    }
+}
